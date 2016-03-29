7a019f3eeb0d96a5e19148e3fc7dcf5c2c2c9bb6
amalia asti hutami <amaliaastihutami@gmail.com>
2016-03-29 10:23:49 +0700

    Added files via upload

---
+++ N02.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package n02;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Praktikum
+ */
+public class N02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    import java.util.Scanner;
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner am = new Scanner(System.in);
+        System.out.println(" masukan angka ");
+        int c = am.nextInt();
+         for(int x=n; x>=1; x--)
+  {
+   for(int y=x-1; y>=1; y--){
+    System.out.print(" ");
+   }
+   for(int z=1; z<=((n+1)-x)*2; z++){
+    System.out.print("*");
+   }
+  System.out.println();
+  }
+ 
+
+    
+}
